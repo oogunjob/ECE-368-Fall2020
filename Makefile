f1a44e1ba6fdb7e1b45fef85ae542636422be877
oogunjob <oogunjob@purdue.edu>
2020-09-11 00:30:22 -0400

    MUCH FASTER LINK SORTING, incorrect number of nodes printed

--- Makefile
+++ Makefile
@@ -15,7 +15,7 @@ pa1: $(OBJS)
 testmemory: pa1
 	$(VAL) ./pa1 -l 15.b output1l
 
-testall: test3 test4 test5
+testall: test1 test2 test3 test4 test5
 
 test1: pa1
 	./pa1 -a 15.b output1a
--- shell_list.c
+++ shell_list.c
@@ -1,23 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+#include <stdbool.h>
 #include "shell_list.h"
 #include "sequence.h"
 #include "list_of_list.h"
 
 static void insertNode(Node* previous, long value);
-
 static void pushList(List ** head);
-static void bubblesort(Node * head);
-static void swap(Node *a, Node *b);
-
 static Node * getNode(Node * head, int index); // *** need to remove
 
-Node * List_Load_From_File(char * filename){
-  // calculates the time taken by function
-  clock_t t; 
-  t = clock(); 
+static List * addSubList(List * head, int k, long *n_comp, bool sortUp);
+static void addAndSort(Node** head, Node* ins, long *n_comp, bool sortUp);
+static Node* removeNode(Node** head);
+static void freeSubList(List * subList);
+static List * addList(Node * sublist);
 
+static List * addList(Node * sublist)
+{
+    List * new = malloc(sizeof(List));
+    new -> node = sublist;
+    new -> next = NULL;
+    return new;
+} 
+
+Node * List_Load_From_File(char * filename){
   FILE * file = fopen(filename, "rb"); // opens binary file of numbers to store in linked list
   
   if(file == NULL){
@@ -46,20 +52,11 @@ Node * List_Load_From_File(char * filename){
     }
   }
 
-  t = clock() - t; 
-  double time_taken = ((double)t)/CLOCKS_PER_SEC; // time taken (in seconds)
-  
-  fprintf(stdout, "Function: Load List took %f seconds to execute.\n", time_taken); // *** need to remove
-
   fclose(file); // closes the file
   return head; // returns the head of the list
 }
 
 Node *List_Shellsort(Node *list, long *n_comp){
-  // calculates the time taken by function
-  clock_t t; 
-  t = clock(); 
-
   int size = 0;  // size of the linked list
   Node* current = list;  // head of the linked list
   
@@ -72,7 +69,6 @@ Node *List_Shellsort(Node *list, long *n_comp){
   int sequenceSize = 0; // number of elements in the sequence
   long *sequence = Generate_2p3q_Seq(size, &sequenceSize); // Pratt's sequence based on the size of the linked list
 
-  // need to implement shell sort of linked list here
   // need to add a clause if the sequence is empty or sequenceSize is still 0 ***
 
   int count = 1; // loop control variable that determines value of k
@@ -80,55 +76,59 @@ Node *List_Shellsort(Node *list, long *n_comp){
   int i; // loop control variable for comparison
   long temp_r; // temporary number
   
-  // given the number in the sequence will determine how many lists I need to create
-  List * head = NULL; // list of lists *** may need to remove
-
-  int listSize = 0; // numbers of lists within the list of lists *** may need to remove
-
-  // creates sequence[sequenceSize - 1] sublists in the list of lists
-  for(listSize = 0; listSize < sequence[sequenceSize - 1]; listSize++){
-    pushList(&head);
-  }
+  k = sequence[sequenceSize - count]; // highest value of k
   
-  List * currentList = head; // current list points the head in the list of lists
-  Node * currentHead = head -> node; // current head points the the first node in the first linked list of linked lists
-  currentList -> node = currentHead; // head of the current list points the 
-  
-  // shell sort implementation (too slow)
-  for(count = (sequenceSize - 1); count >= 0; count--){
-    k = sequence[count]; // selects the value of k from sequence array 
-  
-    for(int j = k; j <= (size - 1); j++){
-      temp_r = getNode(list, j) -> value;
-      i = j;
+  List * head = malloc(sizeof(*head)); // head of list of lists
+  head -> node = list; // first node in the list of lists points to the original list
+  head -> next = head; // head of the list of lists points back to itself
 
-      while(i >= k && getNode(list, i - k) -> value > temp_r){
-        *n_comp += 1; // increments the number of comparisons made
-        getNode(list, i) -> value = getNode(list, i - k) -> value;
-        i = i - k;
-      }
-      getNode(list, i) -> value = temp_r;
+  bool sortUp = false;
+
+  while(k > 1){
+    if(sortUp == false){
+      sortUp = true;
     }
+    else{
+      sortUp = false;
+    }
+
+    head = addSubList(head, k, n_comp, sortUp);
+    k = sequence[sequenceSize - ++(count)];
   }
 
-  // bubblesort(list);
+  list = head -> node;
+  free(head);
 
-  t = clock() - t; 
-  double time_taken = ((double)t)/CLOCKS_PER_SEC; // time taken (in seconds)
-  
-  // fprintf(stdout, "Function: Sort List took %f seconds to execute.\n", time_taken); // *** need to remove
 
-  // deletion of list of lists
-  currentList = head; // current position in linked list
-  List * next; // next list from current list in linked list
+
+  // // shell sort implementation (too slow)
+  // for(count = (sequenceSize - 1); count >= 0; count--){
+  //   k = sequence[count]; // selects the value of k from sequence array 
   
-  while(currentList != NULL){ 
-    next = currentList -> next; 
-    free(currentList); // make sure to delete every not in the list along with the list 
-    currentList = next; 
-  } 
+  //   for(int j = k; j <= (size - 1); j++){
+  //     temp_r = getNode(list, j) -> value;
+  //     i = j;
+
+  //     while(i >= k && getNode(list, i - k) -> value > temp_r){
+  //       *n_comp += 1; // increments the number of comparisons made
+  //       getNode(list, i) -> value = getNode(list, i - k) -> value;
+  //       i = i - k;
+  //     }
+  //     getNode(list, i) -> value = temp_r;
+  //   }
+  // }
+
+  // // deletion of list of lists
+  // currentList = head; // current position in linked list
+  // List * next; // next list from current list in linked list
   
-  currentList = NULL; // ensures list is empty 
+  // while(currentList != NULL){ 
+  //   next = currentList -> next; 
+  //   free(currentList); // make sure to delete every not in the list along with the list 
+  //   currentList = next; 
+  // } 
+  
+  // currentList = NULL; // ensures list is empty 
 
   free(sequence); // frees the sequence array
   return list; 
@@ -155,10 +155,6 @@ static Node * getNode(Node *head, int index){
 } 
 
 int List_Save_To_File(char *filename, Node *list){
-  // calculates the time taken by function
-  clock_t t; 
-  t = clock(); 
-
   FILE * file = fopen(filename, "wb"); // opens binary file that will be written to
  
   // if the linked list is empty, closes the file and returns a count of no elements written
@@ -176,6 +172,7 @@ int List_Save_To_File(char *filename, Node *list){
   while(temp != NULL){
 	// writes the value stored in linked list to the file
 	value = temp -> value;
+  fprintf(stdout, "%ld\n", value);
 	fwrite(&value, sizeof(long), 1, file);   
 	temp = temp -> next;
 	
@@ -191,49 +188,9 @@ int List_Save_To_File(char *filename, Node *list){
     free(current); 
     current = next; 
   } 
-  
-  t = clock() - t; 
-  double time_taken = ((double)t)/CLOCKS_PER_SEC; // time taken (in seconds)
-  
-  // fprintf(stdout, "Function: Save List took %f seconds to execute.\n\n", time_taken); // *** need to remove
+
   fclose(file); // closes the file
   return elements;
-}
-
-static void bubblesort(Node *start){ 
-  int swapped;
-  int i;
-
-  Node * ptr1; 
-  Node * lptr = NULL; 
-  
-  // checks for empty list
-  if (start == NULL) 
-    return; 
-  
-  do
-  { 
-    swapped = 0; 
-    ptr1 = start; 
-  
-    while (ptr1 -> next != lptr){ 
-      if (ptr1 -> value > ptr1 -> next -> value){  
-        swap(ptr1, ptr1 -> next); 
-        swapped = 1; 
-      } 
-      
-      ptr1 = ptr1 -> next; 
-    } 
-      lptr = ptr1; 
-    } while (swapped); 
-} 
-  
-static void swap(Node * a, Node * b){ 
-  long temp = a -> value; 
-  
-  // swaps values or a and b
-  a -> value = b -> value; 
-  b -> value = temp; 
 }
 
 static void insertNode(Node* previous, long value){ 
@@ -242,4 +199,136 @@ static void insertNode(Node* previous, long value){
   
   new_node -> next = previous -> next; // new node's next points to the next of previous node
   previous -> next = new_node; // previous node now points to new node
+}
+
+static List * addSubList(List * head, int k, long *n_comp, bool sortUp){
+    List * prevList = head;
+    List * listptr; // traversal
+    Node * node = NULL;
+    //printf("\npointer to newList %p", (void*)newList);
+    for(int i = 0; i < k; i++)
+    {
+        node = removeNode(&(prevList -> node));
+        if (i == 0)
+        {
+            head = addList(node);
+            listptr = head;
+        }
+        else
+        {
+            listptr -> next = addList(node);
+            listptr = listptr -> next;
+
+            if(node == NULL) // empty
+            {
+                prevList =  prevList ->next;
+                break; 
+            }
+            node -> next = NULL;
+            listptr -> node = node;
+            //printf("%p\n", (void*)listptr);
+        }
+        prevList =  prevList -> next;
+    }
+
+    listptr -> next = head; // circular link
+    if (k == 1) // sort upwards for last sort
+        sortUp = true;
+    // shell sort
+    while(true)
+    {
+        node = removeNode(&( prevList -> node));
+        listptr = listptr -> next;
+        if(node == NULL)
+            break;
+        addAndSort(&(listptr ->node), node,n_comp, sortUp);
+        prevList =  prevList -> next;
+    }
+    freeSubList(prevList);
+    return head;
+}
+
+// add nodes to sublists and sort them
+static void addAndSort(Node** head, Node* ins, long *n_comp, bool sortUp)
+{
+    Node * temp = NULL;
+    temp = *head;
+    (*n_comp)++;
+    if (sortUp == true) // sort upward
+    {
+        if( ins -> value < (*head) -> value )
+        {
+            ins -> next = *head;
+            (*head) = ins;
+            return;
+        }
+        else
+        {
+            while((temp -> next != NULL) && (((temp -> next) -> value) < (ins -> value)))
+            {
+                temp = temp -> next;
+                (*n_comp)++;
+            }
+        }
+    }
+    else // sort downward
+    {
+        if(ins -> value > (*head) -> value)
+        {
+            ins -> next = *head;
+            (*head) = ins;
+            return;
+        }
+
+        while((temp -> next != NULL) && (((temp -> next) -> value) > (ins -> value)))
+        {
+            temp = temp -> next;
+            (*n_comp)++;
+        }
+    }
+    ins -> next = temp -> next;
+    temp -> next = ins;
+
+    return;
+}
+static Node* removeNode(Node** head)
+{
+    if (*head == NULL)
+    {
+        return NULL;
+    }
+    else
+    {
+        Node * removed = *head;
+        *head = (*head) -> next;
+        removed -> next = NULL;
+
+        return removed;
+    }
+}
+
+
+// free sub list nodes
+static void freeSubList(List * subList){
+    if(subList == NULL){
+        return;
+    }
+
+    List * subListNext = subList -> next;
+    if(subListNext == subList)
+    {
+        // empty
+    }
+    else
+    {
+        List * temp = NULL;
+        while((subList != subListNext))
+        {
+            temp = subListNext;
+            subListNext = subListNext -> next;
+            free(temp);
+        }
+    }
+    free(subList);
+    return;
 }
