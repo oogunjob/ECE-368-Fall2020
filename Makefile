8983aea60084994c2abdf9cf648a6c2ec5c664a2
oogunjob <oogunjob@purdue.edu>
2020-09-13 16:15:42 -0400

    necessary changes for submission

--- Makefile
+++ Makefile
@@ -51,5 +51,10 @@ test6: pa1
 
 	./pa1 -l 0.b output6l
 
+test7: pa1
+	./pa1 -b 15.b output7a
+
+	./pa1 -b 15.b output7l	
+
 clean: # remove all machine generated files
 	rm -f pa1 *.o output?
--- pa1.c
+++ pa1.c
@@ -1,15 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-// #include <time.h>
 #include "shell_list.h"
 #include "shell_array.h"
 
 int main(int argc, char * * argv) { 
-  // calculates the time taken by program
-  // clock_t t; 
-  // t = clock(); 
-
   // ensures there are 4 arguments given
   if (argc < 4) {
     fprintf(stderr, "ERROR: Arguments less than 4.\n");
@@ -31,6 +26,8 @@ int main(int argc, char * * argv) {
     fprintf(stdout, "%ld\n", n_comp); // prints number of comparisons to standard output
 
     free(array); // frees memory allocated for the array
+
+    return EXIT_SUCCESS; // exits program
   }
 
   // runs shell sort for linked list
@@ -44,12 +41,14 @@ int main(int argc, char * * argv) {
     
     elements = List_Save_To_File(argv[3], list); // saves the elemments the file and returns the number of elements printed
 	
-    fprintf(stdout, "%ld\n", n_comp); // prints number of comparisons to standard output  
+    fprintf(stdout, "%ld\n", n_comp); // prints number of comparisons to standard output 
+
+    return EXIT_SUCCESS; // exits program 
   }
 
-  // t = clock() - t; 
-  // double time_taken = ((double)t)/CLOCKS_PER_SEC; // time taken (in seconds)
-  
-  // fprintf(stdout, "Program took %f seconds to execute.\n\n", time_taken); 
-  return EXIT_SUCCESS; // return EXIT_SUCCESS is program is successful
+  // if the second argument does not match either a "-a" or "-l", returns EXIT_FAILURE
+  else{
+    fprintf(stderr, "ERROR: Second arguement is invalid.\n");
+    return EXIT_FAILURE;
+  }
 }
